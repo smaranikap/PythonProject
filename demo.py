f6f0751a2b6458f56767e8b3d7315887594965fb
smaranikap <84900954+smaranikap@users.noreply.github.com>
2021-05-28 12:34:17 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,1 +1,11 @@
-print("Hi")
+
+import mysql.connector
+from mysql.connector import errors
+
+mydb = mysql.connector.connect(host='localhost',user='root',password='1234',database='Odia')
+
+mycursor = mydb.cursor()
+mycursor.execute("select * from student")
+result = mycursor.fetchone()
+for i in result:
+    print(i)
