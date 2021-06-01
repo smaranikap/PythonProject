bc0310504377b323aafc7586ddad13f450f2e476
smaranikap <84900954+smaranikap@users.noreply.github.com>
2021-06-01 13:25:38 +0530

    testing 2nd

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Hello")
+mydb = mysql.connector.connect(host='localhost',user='root',password='1234',database='Odia')
+
+mycursor = mydb.cursor()
+mycursor.execute("select * from student")
+result = mycursor.fetchone()
+for i in result:
+    print(i)
